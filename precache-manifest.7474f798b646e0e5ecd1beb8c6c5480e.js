2698984480e3f4bb98a56ab5e255b65081534702
kanjamad bosel <kanjamadpor@gmail.com>
2021-09-11 10:48:33 -0700

    Updates

---
+++ precache-manifest.7474f798b646e0e5ecd1beb8c6c5480e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d13a4deced63c0e79d0388cae1511c6",
+    "url": "/clothing-shop_react_redux/index.html"
+  },
+  {
+    "revision": "867b0010791b5b93ecc6",
+    "url": "/clothing-shop_react_redux/static/css/main.daf8aa86.chunk.css"
+  },
+  {
+    "revision": "c08efbf2dfc53460cd68",
+    "url": "/clothing-shop_react_redux/static/js/2.b9234953.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/clothing-shop_react_redux/static/js/2.b9234953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867b0010791b5b93ecc6",
+    "url": "/clothing-shop_react_redux/static/js/main.4afdf9af.chunk.js"
+  },
+  {
+    "revision": "5ee108ccc41bffea1de0",
+    "url": "/clothing-shop_react_redux/static/js/runtime-main.347f69df.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-shop_react_redux/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-shop_react_redux/static/media/shopping-bag.30f07071.svg"
+  }
+]);
